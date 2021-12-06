80f6516ecd02fd6e440d7a7a8495a4784cb01809
CallyPat <patrick.callahan@sinclair.edu>
2021-12-05 22:19:21 -0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,24 @@
+// Patrick Callahan
+// Cis 1202
+// 12/05/2021
+
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+//class definitions
+class invalidCharacterExcpeption{};
+
+class invalidRangeException{};
+
+class caseConversionException{};
+
+int main() {
+
+	system("pause");
+	return 0;
+}
